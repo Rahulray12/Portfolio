6badc97b2e828d85d3c2466fbc869b0178eecd08
Rahul Ray <rahulray@Rahuls-MacBook-Air.local>
2025-09-18 10:46:33 +0530

    Initial Commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from "../../assets/profile2.png";
+
+const About = () => {
+    return (
+        <section
+            id="about"
+            className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+        >
+            <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+                {/* Left Side */}
+                <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+                    {/* Greeting */}
+                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+                        Hi, I am
+                    </h1>
+                    {/* Name */}
+                    <h2 className="text-4xl sm:text-5xl md:text-5.9xl font-bold text-white mb-4 leading-tight">
+                        Rahul Kumar Ray
+                    </h2>
+                    {/* Skills Heading with Typing Effect */}
+                    <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+                        <span className="text-white">I am a </span>
+                        <ReactTypingEffect
+                            text={[
+                                'Fullstack Developer',
+                                'App Developer',
+                                'UI/UX Designer',
+                                'Power BI Developer',
+                                'Coder',
+                            ]}
+                            speed={100}
+                            eraseSpeed={50}
+                            typingDelay={500}
+                            eraseDelay={2200}
+                            cursorRenderer={(cursor) => (
+                                <span className="text-[#8245ec]">{cursor}</span>
+                            )}
+                        />
+                    </h3>
+                    {/* About Me Paragraph */}
+                    <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+                        I am a passionate full-stack developer currently pursuing my bachelor’s degree.
+                        I have hands-on experience in building web applications using the MERN stack and other modern technologies.
+                        I focus on developing efficient solutions and creating seamless user experiences through both front-end and back-end development.
+                        Eager to learn, grow, and contribute to real-world projects.
+                    </p>
+                    {/* Resume Button */}
+                    <a
+                        href="https://drive.google.com/file/d/1k2qPhQmHNhMWSqHTtx7jclwwPjCKxS0P/view?usp=drive_link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+                        style={{
+                            background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+                            boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+                        }}
+                    >
+                        RESUME
+                    </a>
+
+                </div>
+                {/* Right Side */}
+                <div className="md:w-1/2 flex justify-center md:justify-end">
+                    <Tilt
+                        className="w-50 h-50 sm:w-60 sm:h-60 md:w-[20rem] md:h-[20rem] border-4 border-purple-700 rounded-full"
+                        tiltMaxAngleX={20}
+                        tiltMaxAngleY={20}
+                        perspective={1000}
+                        scale={1.05}
+                        transitionSpeed={1000}
+                        gyroscope={true}
+                    >
+                        <img
+                            src={profileImage}
+                            alt="RAHUL RAY"
+                            className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+                        />
+                    </Tilt>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,308 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import canvalogo from './assets/tech_logo/canva.png';
+import powerbilogo from './assets/tech_logo/Power.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+// import glaLogo from './assets/education_logo/gla_logo.png';
+// import bsaLogo from './assets/education_logo/bsa_logo.png';
+// import vpsLogo from './assets/education_logo/vps_logo.png';
+import sagarLogo from './assets/education_logo/sagarmatha.png';
+import mitLogo from './assets/education_logo/mithila.png';
+import kprLogo from './assets/education_logo/kpriet.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import currcnvtLogo from './assets/work_logo/currency.png';
+import todoLogo from './assets/work_logo/ToDo.png';
+import netflixLogo from './assets/work_logo/Netflix.png';
+import emlsendLogo from './assets/work_logo/Emailsender.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      // { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Canva', logo: canvalogo },
+      { name: 'PowerBI', logo: powerbilogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Intern",
+      company: "Hostly InfoTech",
+      date: "April 2023 - May2023",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    // {
+    //   id: 0,
+    //   img: glaLogo,
+    //   school: "GLA University, Mathura",
+    //   date: "Sept 2022 - July 2024",
+    //   grade: "7.81 CGPA",
+    //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+    //   degree: "Master of Computer Applications - MCA",
+    // },
+    {
+      id: 1,
+      img: kprLogo,
+      school: "KPR INSTITUTE OF ENGINEERING AND TECHNOLOGY",
+      date: "AUG 2022 - 2026",
+      grade: "8.15 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science Engineering (B.E.) from KPR Institute of Engineering and Technology. My studies focus on developing strong technical skills in software development, algorithms, database management, and modern web technologies, preparing me for real-world engineering challenges.",
+      degree: "B.E COMPUTER SCIENCE ENGINEERING",
+    },
+    {
+      id: 2,
+      img: mitLogo,
+      school: "MITHILA INSTITUTE OF TECHNOLOGY, JANAKPUR",
+      date: "APR 2019 - FEB 2021",
+      grade: "78%",
+      desc: "I completed my Class 12 education from Mithila Institute of Technology, under the NEB board, focusing on Science and Computer Science. This gave me a solid foundation in Physics, Chemistry, Mathematics (PCM), and Computer Science, and sparked my passion for technology and software development.",
+      degree: "NEB(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: sagarLogo,
+      school: "SAGARMATHA HIGHER SECONDARY ENGLISH BOARDING SCHOOL, MIRCHAIYA",
+      date: "Apr 2006 - March 2019",
+      grade: "92%",
+      desc: "I completed my class 10 education from SAGARMATHA HIGHER SECONDARY ENGLISH BOARDING SCHOOL, MIRCHAIYA, under the NEB board, where I studied School Level .",
+      degree: "NEB(X), School Level",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "To-Do App",
+      description:"A powerful and user-friendly React.js application designed to organize and manage daily tasks with ease. Simply add, edit, or delete tasks, and the app keeps everything neatly structured, including task status and priorities. The intuitive interface ensures a seamless experience, making it an essential productivity tool for students, professionals, and anyone looking to stay organized.",
+      image: todoLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/Rahulray12/To-do-App",
+      webapp: "https://rahulray12.github.io/To-do-App/",
+    },
+    {
+      id: 1,
+      title: "Currency Converter",
+      description:"A full-stack currency converter platform designed to provide real-time exchange rates and seamless conversions between multiple currencies. The platform allows users to quickly convert values, view historical trends, and access accurate financial data, making it an essential tool for students, travelers, and professionals managing global transactions.",
+      image: currcnvtLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://rahulray12.github.io/Currency-Converter/",
+      webapp: "https://github.com/Rahulray12/Currency-Converter",
+    },
+    {
+      id: 2,
+      title: "Hybrid Netflix Clone",
+      description:"A React-based web application that replicates the core features of Netflix, offering a wide collection of movies and TV shows with categories, trending lists, and detailed previews. The intuitive design and smooth streaming-like experience make it an engaging platform for entertainment enthusiasts.",
+      image: netflixLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rahul12/netflix-clone/",
+      webapp: "https://github.io/Rahul12/nefix.io.com/",
+    },
+    {
+      id: 3,
+      title: "Email Sender",
+      description:"An efficient and customizable NPM package for sending emails seamlessly. Built using React.js and Node.js, it provides robust features like SMTP integration, template customization, and error handling, enabling developers to easily integrate reliable email functionality into their applications.",
+      image: emlsendLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/Rahul12/netflix-clone/",
+      webapp: "https://github.io/Rahul12/nefix.io.com/",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    // {
+    //   id: 5,
+    //   title: "Webverse Digital",
+    //   description:
+    //     "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    //   image: webverLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    //   github: "https://github.com/codingmastr/Webverse-Digital",
+    //   webapp: "https://webversedigital.com/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
