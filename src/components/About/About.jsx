edcfa94ccda670cb425f54261af94f4e7d30f04e
Rahul Ray <rahulray@Rahuls-MacBook-Air.local>
2026-07-16 19:55:54 +0545

    The resume link was updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
                     </p>
                     {/* Resume Button */}
                     <a
-                        href="https://drive.google.com/file/d/1k2qPhQmHNhMWSqHTtx7jclwwPjCKxS0P/view?usp=drive_link"
+                        href="https://drive.google.com/file/d/1lsPQ2osjx09t4bUJfBGIFCahXDlLZofu/view?usp=drive_link"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
